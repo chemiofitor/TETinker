3ee13fcfcd9cab517fdf0c6f7f766f75ba303497
chemiofitor <98449295+chemiofitor@users.noreply.github.com>
2024-02-04 23:30:47 +0800

    ddd

--- src/main/java/ho/artisan/tetinker/TETinker.java
+++ src/main/java/ho/artisan/tetinker/TETinker.java
@@ -5,7 +5,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod("tetinker")
 public class TETinker {
     private static final Logger LOGGER = LogUtils.getLogger();
