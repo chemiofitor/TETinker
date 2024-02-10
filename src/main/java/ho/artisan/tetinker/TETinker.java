acf94c33d1efd615b67cff8f0e1eb35dddafc7b3
chemiofitor <98449295+chemiofitor@users.noreply.github.com>
2024-02-10 23:28:19 +0800

    1.1 updata

--- src/main/java/ho/artisan/tetinker/TETinker.java
+++ src/main/java/ho/artisan/tetinker/TETinker.java
@@ -1,19 +1,16 @@
 package ho.artisan.tetinker;
 
-import com.mojang.logging.LogUtils;
 import ho.artisan.tetinker.init.TETiFluids;
 import ho.artisan.tetinker.init.TETiModifiers;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 
 @Mod("tetinker")
 public class TETinker {
     public static final String MOD_ID = "tetinker";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public TETinker() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -22,7 +19,5 @@ public class TETinker {
         TETiFluids.FLUIDS.register(eventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
-
-        LOGGER.info("TETinker has setup!");
     }
 }
